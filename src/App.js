fb14b9f2606c3626adddd7785617b1840855d7c0
MaximusCipher <113954406+evehnkadusale@users.noreply.github.com>
2024-05-26 01:35:56 +0800

    install tailwindcss
    install tailwindcss

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         </p>
         <a
           className="App-link"
